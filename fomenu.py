6b70e133fcf41293f6359528ddc27a67dde8721f
DaW33D <zsebokdavidf@gmail.com>
2021-04-29 13:47:17 +0200

    gombok pocízionálása

--- fomenu.py
+++ fomenu.py
@@ -13,26 +13,23 @@ class Game:
     
     root =  Tk()
     root.geometry("900x600")
-    root.rowconfigure(2, weight=1)
+    root.rowconfigure(4, weight=1)
     root.configure(bg="black")
     root.resizable(0, 0)
     root.title("Race 2021")
 
     jatek = Button(text="Play", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    jatek.pack()
-    """jatek.grid(row=1, column=0, columnspan=2, padx=1, pady=1)"""
+    jatek.grid(row=0, column=0, padx=360, pady=20)
 
     credits = Button(text="Credits", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    credits.pack()
-    """credits.grid(row=2, column=0, columnspan=2, padx=1, pady=1)"""
+    credits.grid(row=1, column=0, padx=360, pady=20)
 
     options = Button(text="Options", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    options.pack()
-    """options.grid(row=3, column=0, columnspan=2, padx=1, pady=1)"""
+    options.grid(row=2, column=0, padx=360, pady=20)
 
     exit = Button(text='Exit', fg="Red", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=root.destroy)
-    exit.pack()
-    """exit.grid(row=4, column=0, columnspan=2, padx=1, pady=1)"""
+    exit.grid(row=3, column=0, padx=360, pady=20)
+
     root.mainloop()
 
 Game()
