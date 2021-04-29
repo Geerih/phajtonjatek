06de214e5df361e2adaa19d24975ef0f5df5c774
Benceplays <nemeth.csaba.bence@gmail.com>
2021-04-29 12:36:29 +0200

    Dávid wezt csak neked csak most

--- fomenu.py
+++ fomenu.py
@@ -18,9 +18,9 @@ class Game:
     root.configure(bg = "black")
     root.resizable(0, 0)
     root.title("Race 2021")
-    jatek = Button(text = "Play", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 0, column = 0, columnspan = 2, padx = 1, pady = 1)
-    credits = Button(text = "Credits", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 1, column = 0, columnspan = 2, padx = 1, pady = 1)
-    options = Button(text = "Options", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 2, column = 0, columnspan = 2, padx = 1, pady = 1)
+    jatek = Button(text = "Play", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 0, column = 0, columnspan = 2, padx = 400, pady = 1)
+    credits = Button(text = "Credits", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 1, column = 0, columnspan = 2, padx = 400, pady = 1)
+    options = Button(text = "Options", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 2, column = 0, columnspan = 2, padx = 400, pady = 1)
     exit = Button(root, text='Exit', fg = "Red", command = root.destroy)
     root.mainloop()
 
