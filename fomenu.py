45d6cc3c504a7ff25e0e0b5a9a9a718049ce0077
DaW33D <zsebokdavidf@gmail.com>
2021-04-29 12:51:47 +0200

    gombok helyretevése + kód szépítése

--- fomenu.py
+++ fomenu.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import tkinter
 
 class Game:
     def game_click():
@@ -14,14 +13,22 @@ class Game:
     
     root =  Tk()
     root.geometry("900x600")
-    root.rowconfigure(2,weight=1)
-    root.configure(bg = "black")
+    root.rowconfigure(2, weight=1)
+    root.configure(bg="black")
     root.resizable(0, 0)
     root.title("Race 2021")
-    jatek = Button(text = "Play", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 0, column = 0, columnspan = 2, padx = 1, pady = 1)
-    credits = Button(text = "Credits", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 1, column = 0, columnspan = 2, padx = 1, pady = 1)
-    options = Button(text = "Options", fg = "black", width = 20, height = 5, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 2, column = 0, columnspan = 2, padx = 1, pady = 1)
-    exit = Button(root, text='Exit', fg = "Red", command = root.destroy)
+
+    jatek = Button(text="Play", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
+    jatek.grid(row=0, column=0, columnspan=2, padx=1, pady=1)
+
+    credits = Button(text="Credits", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
+    credits.grid(row=1, column=0, columnspan=2, padx=1, pady=1)
+
+    options = Button(text="Options", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
+    options.grid(row=2, column=0, columnspan=2, padx=1, pady=1)
+
+    exit = Button(root, text='Exit', fg="Red", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=root.destroy)
+    exit.grid(row=3, column=0, columnspan=2, padx=1, pady=1)
     root.mainloop()
 
 Game()
