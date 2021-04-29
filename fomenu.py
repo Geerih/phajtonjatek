8075c2c8d59147df1dfe07ce2f8b85e881902be5
DaW33D <zsebokdavidf@gmail.com>
2021-04-29 13:00:10 +0200

    gombok pocízionálása

--- fomenu.py
+++ fomenu.py
@@ -19,16 +19,20 @@ class Game:
     root.title("Race 2021")
 
     jatek = Button(text="Play", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    jatek.grid(row=0, column=0, columnspan=2, padx=1, pady=1)
+    jatek.pack()
+    """jatek.grid(row=1, column=0, columnspan=2, padx=1, pady=1)"""
 
     credits = Button(text="Credits", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    credits.grid(row=1, column=0, columnspan=2, padx=1, pady=1)
+    credits.pack()
+    """credits.grid(row=2, column=0, columnspan=2, padx=1, pady=1)"""
 
     options = Button(text="Options", fg="black", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=lambda: btn_click(0))
-    options.grid(row=2, column=0, columnspan=2, padx=1, pady=1)
+    options.pack()
+    """options.grid(row=3, column=0, columnspan=2, padx=1, pady=1)"""
 
-    exit = Button(root, text='Exit', fg="Red", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=root.destroy)
-    exit.grid(row=3, column=0, columnspan=2, padx=1, pady=1)
+    exit = Button(text='Exit', fg="Red", width=20, height=5, bd=0, bg="#fff", cursor="hand2", command=root.destroy)
+    exit.pack()
+    """exit.grid(row=4, column=0, columnspan=2, padx=1, pady=1)"""
     root.mainloop()
 
 Game()
