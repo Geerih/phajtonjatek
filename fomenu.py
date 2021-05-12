701c5ce6cf7230821a6ae12aad1e3c437487530f
daweed <zsebokdavidf@gmail.com>
2021-05-12 21:13:49 +0200

    főmenü gombok + credit szarakodás

--- fomenu.py
+++ fomenu.py
@@ -1,3 +1,4 @@
+import tkinter.font
 from tkinter import *
 
 class Game:
@@ -21,23 +22,28 @@ class Game:
     root.configure(bg="black")
     root.resizable(0, 0)
     root.title("Pro Street Racer")
+    buttonfont = tkinter.font.Font(size=30, weight="bold")
 
-    jatek = Button(text="Play", fg="black", width=40, height=6, bd=0, bg="#f1f5ff", cursor="hand2", command=lambda: btn_click(0))
+    jatek = Button(text="Play", fg="black", width=13, height=1, bd=0, bg="#f1f5ff", cursor="hand2", command=lambda: btn_click(0))
     jatek.grid(row=0, column=0, padx=1265, pady=20)
+    jatek['font'] = buttonfont
 
-    credits = Button(text="Credits", fg="black", width=40, height=6, bd=0, bg="#f1f5ff", cursor="hand2", command=lambda: btn_click(0))
+    credits = Button(text="Credits", fg="black", width=13, height=1, bd=0, bg="#f1f5ff", cursor="hand2", command=credits_click())
     credits.grid(row=1, column=400, padx=1265, pady=20)
+    credits['font'] = buttonfont
 
-    options = Button(text="Options", fg="black", width=40, height=6, bd=0, bg="#f1f5ff", cursor="hand2", command=lambda: btn_click(0))
+    options = Button(text="Options", fg="black", width=13, height=1, bd=0, bg="#f1f5ff", cursor="hand2", command=lambda: btn_click(0))
     options.grid(row=2, column=0, padx=1265, pady=20)
+    options['font'] = buttonfont
 
-    exit = Button(text='Exit', fg="Red", width=40, height=6, bd=0, bg="#f1f5ff", cursor="hand2", command=root.destroy)
+    exit = Button(text='Exit', fg="Red", width=13, height=1, bd=0, bg="#f1f5ff", cursor="hand2", command=root.destroy)
     exit.grid(row=3, column=0, padx=1265, pady=20)
+    exit['font'] = buttonfont
 
-    jatek.place(x=root.winfo_screenwidth() / 100 * 65.78125, y=root.winfo_screenheight() / 100 * 37.962)
-    credits.place(x=root.winfo_screenwidth() / 100 * 65.78125, y=root.winfo_screenheight() / 100 * 51.481)
-    options.place(x=root.winfo_screenwidth() / 100 * 65.78125, y=root.winfo_screenheight() / 100 * 65.370)
-    exit.place(x=root.winfo_screenwidth() / 100 * 65.78125, y=root.winfo_screenheight() / 100 * 79.629)
+    jatek.place(x=root.winfo_screenwidth() / 100 * 64.739583, y=root.winfo_screenheight() / 100 * 38.962)
+    credits.place(x=root.winfo_screenwidth() / 100 * 64.739583, y=root.winfo_screenheight() / 100 * 52.481)
+    options.place(x=root.winfo_screenwidth() / 100 * 64.739583, y=root.winfo_screenheight() / 100 * 66.370)
+    exit.place(x=root.winfo_screenwidth() / 100 * 64.739583, y=root.winfo_screenheight() / 100 * 80.629)
     root.mainloop()
 
 Game()
