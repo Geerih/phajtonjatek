8552af36a70f3b9b19a379cf8f59b61b9719a94c
Da-WEED <zsebokdavidf@gmail.com>
2021-05-26 11:13:29 +0200

    test fullscreen (buzi vagyél, mkijöoiiojkjiuiioj9ihoiuhoihoihoi)

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 from n_mygameworld import *
 import pgzrun
+import tkinter as tk
 
 menustage = MyStage()
 
-WIDTH = 1920
-HEIGHT = 1080
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+WIDTH = screen_width
+HEIGHT = screen_height
+DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 m1 = MyActor(image="kezdokep.png", pos=(0, 0), anchor=(0, 0))
-m1.set_width(1920, 1080)
+m1.set_size(width=screen_width, height=screen_height)
 menustage.add_actor(m1)
 
 def update(dt):
