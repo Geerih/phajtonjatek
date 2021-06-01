74678fab9e37a64e857641e3f9526dd398d0d4ea
Roland1520 <troznairoland@gmail.com>
2021-06-01 13:53:24 +0200

    kész a credit gyerekik

--- credit.py
+++ credit.py
@@ -1,5 +1,3 @@
-import self as self
-
 from n_mygameworld import *
 import pgzrun
 import tkinter as tk
@@ -19,26 +17,29 @@ menustage.add_actor(m1)
 
 # label
 
-
+def exit():
+    exit()
 
 def update(dt):
     menustage.update(dt)
 
+def on_mouse_down(pos, button):
+    menustage.on_mouse_down(pos, button)
 
 def draw():
     menustage.draw()
 
 text1: MyButton = MyButton()
-text1.set_x(400)
+text1.set_x(650)
 text1.set_y(80)
 text1.set_fontsize(80)
 text1.set_color(b=255, g=255, r=255)
 text1.set_rotation(0)
-text1.set_text("Fejlesztők / Készítők:")
+text1.set_text("Developers / Creators:")
 menustage.add_actor(text1)
 
 text2: MyButton = MyButton()
-text2.set_x(540)
+text2.set_x(810)
 text2.set_y(210)
 text2.set_fontsize(40)
 text2.set_color(b=255, g=255, r=255)
@@ -47,7 +48,7 @@ text2.set_text("Troznai Roland")
 menustage.add_actor(text2)
 
 text3: MyButton = MyButton()
-text3.set_x(540)
+text3.set_x(810)
 text3.set_y(290)
 text3.set_fontsize(40)
 text3.set_color(b=255, g=255, r=255)
@@ -56,7 +57,7 @@ text3.set_text("Németh Csaba Bence")
 menustage.add_actor(text3)
 
 text4: MyButton = MyButton()
-text4.set_x(540)
+text4.set_x(810)
 text4.set_y(370)
 text4.set_fontsize(40)
 text4.set_color(b=255, g=255, r=255)
@@ -65,12 +66,22 @@ text4.set_text("Zsebők Dávid Ferenc")
 menustage.add_actor(text4)
 
 text5: MyButton = MyButton()
-text5.set_x(540)
+text5.set_x(810)
 text5.set_y(450)
 text5.set_fontsize(40)
 text5.set_color(b=255, g=255, r=255)
 text5.set_rotation(0)
 text5.set_text("Oláh Gergő László")
 menustage.add_actor(text5)
+
+text1: MyButton = MyButton()
+text1.set_x(1550)
+text1.set_y(980)
+text1.set_fontsize(50)
+text1.set_color(b=50, g=50, r=210)
+text1.set_rotation(0)
+text1.set_text("Back to main menu")
+text1.set_on_mouse_down_listener(exit)
+menustage.add_actor(text1)
 
 pgzrun.go()
