331649b7d11d97aea2917a2ff9d142e9030958f4
DaW33D <zsebokdavidf@gmail.com>
2021-06-03 18:24:57 +0200

    már nem emlékszem mit változtattam lol

--- credit.py
+++ credit.py
@@ -1,76 +1,78 @@
-import self as self
-
 from n_mygameworld import *
 import pgzrun
 import tkinter as tk
 
-menustage = MyStage()
 
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-WIDTH = screen_width
-HEIGHT = screen_height
-DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+class Creditstage(MyStage):
 
-m1 = MyActor(image="credit.png", pos=(0, 0), anchor=(0, 0))
-m1.set_size(width=screen_width, height=screen_height)
-menustage.add_actor(m1)
+    def __init__(self):
+        super().__init__()
+        root = tk.Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        WIDTH = screen_width
+        HEIGHT = screen_height
+        DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-# label
+        m1 = MyActor(image="credit.jpg", pos=(0, 0), anchor=(0, 0))
+        m1.set_size(width=screen_width, height=screen_height)
+        self.add_actor(m1)
+
+        text1: MyLabel = MyLabel()
+        text1.set_x(400)
+        text1.set_y(80)
+        text1.set_fontsize(80)
+        text1.set_color(b=255, g=255, r=255)
+        text1.set_rotation(0)
+        text1.set_text("Fejlesztők / Készítők:")
+        self.add_actor(text1)
+
+
+        text2: MyLabel = MyLabel()
+        text2.set_x(540)
+        text2.set_y(210)
+        text2.set_fontsize(40)
+        text2.set_color(b=255, g=255, r=255)
+        text2.set_rotation(0)
+        text2.set_text("Troznai Roland")
+        self.add_actor(text2)
+
+        text3: MyLabel = MyLabel()
+        text3.set_x(540)
+        text3.set_y(290)
+        text3.set_fontsize(40)
+        text3.set_color(b=255, g=255, r=255)
+        text3.set_rotation(0)
+        text3.set_text("Németh Csaba Bence")
+        self.add_actor(text3)
+
+        text4: MyLabel = MyLabel()
+        text4.set_x(540)
+        text4.set_y(370)
+        text4.set_fontsize(40)
+        text4.set_color(b=255, g=255, r=255)
+        text4.set_rotation(0)
+        text4.set_text("Zsebők Dávid Ferenc")
+        self.add_actor(text4)
+
+
+        text5: MyLabel = MyLabel()
+        text5.set_x(540)
+        text5.set_y(450)
+        text5.set_fontsize(40)
+        text5.set_color(b=255, g=255, r=255)
+        text5.set_rotation(0)
+        text5.set_text("Oláh Gergő László")
+        self.add_actor(text5)
+
+
+    # label
+
+    def update(self, delta_time: float = 0.0166666666666666666666):
+        self.update(delta_time)
+
+    def draw(self):
+        self.draw()
 
 
 
-def update(dt):
-    menustage.update(dt)
-
-
-def draw():
-    menustage.draw()
-
-text1: MyButton = MyButton()
-text1.set_x(400)
-text1.set_y(80)
-text1.set_fontsize(80)
-text1.set_color(b=255, g=255, r=255)
-text1.set_rotation(0)
-text1.set_text("Fejlesztők / Készítők:")
-menustage.add_actor(text1)
-
-text2: MyButton = MyButton()
-text2.set_x(540)
-text2.set_y(210)
-text2.set_fontsize(40)
-text2.set_color(b=255, g=255, r=255)
-text2.set_rotation(0)
-text2.set_text("Troznai Roland")
-menustage.add_actor(text2)
-
-text3: MyButton = MyButton()
-text3.set_x(540)
-text3.set_y(290)
-text3.set_fontsize(40)
-text3.set_color(b=255, g=255, r=255)
-text3.set_rotation(0)
-text3.set_text("Németh Csaba Bence")
-menustage.add_actor(text3)
-
-text4: MyButton = MyButton()
-text4.set_x(540)
-text4.set_y(370)
-text4.set_fontsize(40)
-text4.set_color(b=255, g=255, r=255)
-text4.set_rotation(0)
-text4.set_text("Zsebők Dávid Ferenc")
-menustage.add_actor(text4)
-
-text5: MyButton = MyButton()
-text5.set_x(540)
-text5.set_y(450)
-text5.set_fontsize(40)
-text5.set_color(b=255, g=255, r=255)
-text5.set_rotation(0)
-text5.set_text("Oláh Gergő László")
-menustage.add_actor(text5)
-
-pgzrun.go()
--- fomenu.py
+++ fomenu.py
@@ -1,10 +1,12 @@
 from n_mygameworld import *
 import pgzrun
 import tkinter as tk
+import credit as ct
 
 menustage = MyStage()
 
 # screen
+
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -12,37 +14,88 @@ WIDTH = screen_width
 HEIGHT = screen_height
 DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-# actorok
+
+# actors
+
 m1 = MyActor(image="kezdokep.png", pos=(0, 0), anchor=(0, 0))
 m1.set_size(width=screen_width, height=screen_height)
 menustage.add_actor(m1)
 
+
+# function
+
+def exit(pos=0, btn=0):
+    print('exit')
+    exit()
+
+
+def startgame(pos=0, btn=0):
+    print('play')
+
+
+def credit(self, pos=0, btn=0):
+    print('credit')
+    ct.Creditstage()
+
+
+
+
+def options(pos=0, btn=0):
+    print('options')
+
+
 # label
+
 text1: MyButton = MyButton()
-text1.set_x(900)
-text1.set_y(310)
+text1.set_x(1650)
+text1.set_y(400)
+text1.set_height(115)
+text1.set_fontsize(50)
 text1.set_fontsize(50)
 text1.set_color(b=0, g=0, r=0)
 text1.set_rotation(0)
-text1.set_text("EXIT")
-text1.set_on_mouse_down_listener(exit)
+text1.set_text("PLAY")
+text1.set_on_mouse_down_listener(func=startgame)
 menustage.add_actor(text1)
 
 text2: MyButton = MyButton()
-text2.set_x(900)
-text2.set_y(310)
+text2.set_x(1650)
+text2.set_y(547)
+text2.set_height(115)
+text2.set_fontsize(50)
 text2.set_fontsize(50)
 text2.set_color(b=0, g=0, r=0)
 text2.set_rotation(0)
-text2.set_text("PLAY")
-text2.set_on_mouse_down_listener(startgame)
+text2.set_text("CREDIT")
+text2.set_on_mouse_down_listener(func=credit)
 menustage.add_actor(text2)
 
-def startgame():
-    print('play')
+text3: MyButton = MyButton()
+text3.set_x(1650)
+text3.set_y(695)
+text3.set_height(115)
+text3.set_fontsize(50)
+text3.set_fontsize(50)
+text3.set_color(b=0, g=0, r=0)
+text3.set_rotation(0)
+text3.set_text("OPTIONS")
+text3.set_on_mouse_down_listener(func=options)
+menustage.add_actor(text3)
 
-def exit():
-    exit()
+text4: MyButton = MyButton()
+text4.set_x(1650)
+text4.set_y(847)
+text4.set_width(100)
+text4.set_height(115)
+text4.set_fontsize(50)
+text4.set_color(b=0, g=0, r=0)
+text4.set_rotation(0)
+text4.set_text("EXIT")
+text4.set_on_mouse_down_listener(func=exit)
+menustage.add_actor(text4)
+
+
+# other
 
 def update(dt):
     menustage.update(dt)
@@ -50,6 +103,7 @@ def update(dt):
 
 def draw():
     menustage.draw()
+
 
 def on_mouse_down(pos, button):
     menustage.on_mouse_down(pos, button)
